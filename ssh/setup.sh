92fffbf42830ca662112d51b32cdb9712bb3e4c5
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-07 00:21:20 +0200

    test

--- ssh/setup.sh
+++ ssh/setup.sh
@@ -7,8 +7,8 @@ apt-get install -y --no-install-recommends ssh
 cat > $start_script << EOT
 #!/bin/bash -x
 
+mkdir -p /var/run/sshd
 /usr/sbin/sshd -D -e -f /etc/ssh/sshd_config
-sleep 3600
 EOT
 
 chmod 700 $start_script
