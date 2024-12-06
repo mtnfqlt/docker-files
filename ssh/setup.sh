c4d52689e7f580d6f9bab8d14638a2c692cfe6a1
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-07 00:09:08 +0200

    test

--- ssh/setup.sh
+++ ssh/setup.sh
@@ -1,8 +1,14 @@
-#!/bin/bash -x
+#!/bin/bash -e
 
 start_script='/srv/start.sh'
 
 apt-get install -y --no-install-recommends ssh
-echo 'sshd -D -e -f /etc/ssh/sshd_config' > $start_script
-chmod 700 $start_script
+
+cat > $start_script << EOT
+#!/bin/bash -x
+
+sshd -D -e -f /etc/ssh/sshd_config
 sleep 3600
+EOT
+
+chmod 700 $start_script
