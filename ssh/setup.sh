82476f69f596b49ce0cd1e24f0216ca5d06fee90
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-07 00:01:31 +0200

    test

--- ssh/setup.sh
+++ ssh/setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -e
 
+start_script='/srv/start.sh'
+
 apt-get install -y --no-install-recommends ssh
+echo 'sshd -D -e -f /etc/ssh/sshd_config' > $start_script
+chmod 700 $start_script
--- tools/setup.sh
+++ tools/setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -e
 
-# Sets debconf to use non-interactive mode for package installation
-echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
-
 apt-get update
 apt-get full-upgrade -y
 
