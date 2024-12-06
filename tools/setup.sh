354c95f3fb2868199e79740367fccef2abdb4910
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-06 22:40:23 +0200

    test

---
+++ tools/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+printf '\033[1;32mtools/setup.sh\033[0m\n'
+
+# Sets debconf to use non-interactive mode for package installation
+echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+
+apt-get update
+apt-get full-upgrade -y
+
+apt-get install -y --no-install-recommends \
+  apt-utils \
+  iputils-ping \
+  mc \
+  net-tools \
+  procps
