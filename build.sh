f46e77c78524b5569c574e0195cdbf01b398a6fe
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-07 17:03:08 +0200

    test

--- build.sh
+++ build.sh
@@ -7,7 +7,8 @@ setup_script_name='setup.sh'
 
 cd "$work_dir"
 mkdir -p "$file_dir"
-wget -nv --no-continue $repo_url/Dockerfile $repo_url/$setup_script_name -P "$file_dir"
+#wget -nv --no-continue $repo_url/Dockerfile $repo_url/$setup_script_name -P "$file_dir"
+wget -nv -N -P "$file_dir" $repo_url/Dockerfile $repo_url/$setup_script_name
 chmod 700 "$file_dir/$setup_script_name"
 
 # docker compose down
