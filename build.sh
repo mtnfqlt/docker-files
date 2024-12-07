008572e6f5a2f8c94fa7202087da8ee9ae2ff455
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-07 18:27:07 +0200

    test

--- build.sh
+++ build.sh
@@ -1,20 +1,20 @@
 #!/bin/bash -e
 
-# work_dir=$(dirname "$(realpath "$1")")
-# file_dir=$work_dir/files
-# repo_url='https://raw.githubusercontent.com/mtnfqlt/docker-files/refs/heads/main'
-# setup_script_name='setup.sh'
+work_dir=$(dirname "$(realpath "$1")")
+file_dir=$work_dir/files
+repo_url='https://raw.githubusercontent.com/mtnfqlt/docker-files/refs/heads/main'
+setup_script_name='setup.sh'
 
-# mkdir -p "$file_dir"
-# cd "$file_dir"
+mkdir -p "$file_dir"
+cd "$file_dir"
 
-# curl -sS -H 'Cache-Control: no-cache' \
-#   -o ./Dockerfile $repo_url/Dockerfile \
-#   -o ./$setup_script_name $repo_url/$setup_script_name
+curl -sS -H 'Cache-Control: no-cache' \
+  -o ./Dockerfile $repo_url/Dockerfile \
+  -o ./$setup_script_name $repo_url/$setup_script_name
 
-# chmod 700 ./$setup_script_name
+chmod 700 ./$setup_script_name
 
-# cd "$work_dir"
-# docker compose down
-# docker compose --progress=plain build --no-cache --pull
-# docker compose up
+cd "$work_dir"
+docker compose down
+docker compose --progress=plain build --no-cache --pull
+docker compose up
