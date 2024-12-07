bf8ec5ae4553edb704d60285d29609ccbcc29fc0
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-07 19:32:47 +0200

    test

--- build.sh
+++ build.sh
@@ -4,26 +4,17 @@ work_dir=$(dirname "$(realpath "$1")")
 repo_url=$2
 branch=$3
 
-echo "$work_dir"
-echo "$repo_url"
-echo "$branch"
+cd "$work_dir/files"
 
 for file in Dockerfile setup.sh; do
   curl -sS -H 'Cache-Control: no-cache, no-store' "$repo_url/$file?ref=$branch" | \
     jq -r '.content' | \
-    base64 -d > ./files/$file
+    base64 -d > ./$file
 done
 
-# file_dir=$work_dir/files
-# repo_url='https://raw.githubusercontent.com/mtnfqlt/docker-files/refs/heads/main'
-# setup_script_name='setup.sh'
+chmod 700 ./setup.sh
 
-# mkdir -p "$file_dir"
-# cd "$file_dir"
-# curl -sS -H 'Cache-Control: no-cache' -O $repo_url/Dockerfile -O $repo_url/$setup_script_name
-# chmod 700 ./$setup_script_name
-
-# cd "$work_dir"
-# docker compose down
-# docker compose --progress=plain build --no-cache --pull
-# docker compose up
+cd "$work_dir"
+docker compose down
+docker compose --progress=plain build --no-cache --pull
+docker compose up
