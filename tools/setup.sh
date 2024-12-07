1635582164c14e4a02899bbc43e6534f96a23a56
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-07 23:29:56 +0200

    test

--- tools/setup.sh
+++ tools/setup.sh
@@ -2,7 +2,12 @@
 
 apt-get install -y --no-install-recommends \
   apt-utils \
+  dnsutils \
+  iproute2 \
   iputils-ping \
   mc \
+  netcat-traditional \
   net-tools \
-  procps
+  procps \
+  telnet \
+  wget
