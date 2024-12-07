16939b332125f1408d46ef141ca501b3c9802acc
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-07 17:32:27 +0200

    test

--- build.sh
+++ build.sh
@@ -5,11 +5,12 @@ file_dir=$work_dir/files
 repo_url='https://raw.githubusercontent.com/mtnfqlt/docker-files/refs/heads/main'
 setup_script_name='setup.sh'
 
-cd "$work_dir"
 mkdir -p "$file_dir"
-wget -nv -N -P "$file_dir" $repo_url/Dockerfile $repo_url/$setup_script_name
-chmod 700 "$file_dir/$setup_script_name"
+cd "$file_dir"
+curl -sS -O $repo_url/Dockerfile -O $repo_url/$setup_script_name
+chmod 700 ./$setup_script_name
 
+cd "$work_dir"
 docker compose down
 docker compose --progress=plain build --no-cache --pull
 docker compose up
