1c882d18dda5b79c8777187968bb0027f77c7dc6
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-07 20:27:27 +0200

    test

--- setup.sh
+++ setup.sh
@@ -1,10 +1,17 @@
 #!/bin/bash -e
 
-echo "$1"
-echo "$2"
-echo "$3"
-echo "$4"
-echo "$5"
+repo_url="$1"
+branch="$2"
+setup_list="$3"
+pre_setup_script="$4"
+post_setup_script="$5"
+
+echo "$repo_url"
+echo "$branch"
+echo "$setup_list"
+echo "$pre_setup_script"
+echo "$post_setup_script"
+
 
 # repo_url='https://raw.githubusercontent.com/mtnfqlt/docker-files/refs/heads/main'
 
