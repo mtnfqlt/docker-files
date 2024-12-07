2302b8ee371e39da2babb8ef4a9026a7caf5c8e6
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-07 16:20:17 +0200

    test

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
-docker compose down
-docker compose --progress=plain build --no-cache --pull
-docker compose up
+work_dir=$(dirname "$(realpath "$1")")
+
+echo "$work_dir"
+# docker compose down
+# docker compose --progress=plain build --no-cache --pull
+# docker compose up
