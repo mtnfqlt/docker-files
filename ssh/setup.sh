6b4b134cbaebaab1ab09cf379d65a570ef385bd9
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-08 02:16:52 +0200

    test

--- ssh/setup.sh
+++ ssh/setup.sh
@@ -31,7 +31,7 @@ authorized_keys_file='$authorized_keys_file'
 
 rm -f \$authorized_keys_file
 find $ssh_dir -maxdepth 1 -type f -name '*.pub' -exec cat {} + >> \$authorized_keys_file
-ip route get 8.8.8.8 | awk '{print \$7}'
+echo Container IP "$(ip route get 8.8.8.8 | head -1 | awk '{print \$7}')"
 /usr/sbin/sshd -Def /etc/ssh/sshd_config
 EOT
 
