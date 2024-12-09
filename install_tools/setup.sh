df6525923fdae083f0795606d46155507e844950
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-09 21:47:38 +0200

    test

--- install_tools/setup.sh
+++ install_tools/setup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -e
 
-# # shellcheck disable=SC1091
-# source ../include.src
+# shellcheck disable=SC1091
+source ../include.src
 
-# # shellcheck disable=SC2154
-# echo "$list"
-
-echo "$@"
+# shellcheck disable=SC2154
+echo "$list"
