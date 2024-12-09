a7a06383f847a82f0f8b566bd6fc788e7bdbbfde
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-10 00:59:58 +0200

    test

--- setup.sh
+++ setup.sh
@@ -15,9 +15,9 @@ apt-get update
 apt-get full-upgrade -y
 apt-get install -y --no-install-recommends apt-utils jq
 
-echo "$setup_list" | jq -r 'to_entries[] | "\(.key) \(.value)"' | while read -r dir zarg; do
+echo "$setup_list" | jq -r 'to_entries[] | "\(.key) \(.value)"' | while read -r ddir zarg; do
   #"./$dir/setup.sh" "$arg"
-  echo "$dir"
+  echo "$ddir"
   echo "$zarg"
   "$dir" $zarg
 done
