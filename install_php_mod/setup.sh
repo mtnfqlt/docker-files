3132518179ab99672546a8ccea1459119f7f6b05
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-10 00:47:03 +0200

    test

--- install_php_mod/setup.sh
+++ install_php_mod/setup.sh
@@ -8,6 +8,13 @@ source ./include.src
 # shellcheck disable=SC2154
 echo "$list"
 echo "$@"
+
+while [ $# -gt 0 ]; do
+    echo "Arg: $1"
+    shift
+done
+echo "Po ciklo: $@"
+
 pecl install redis-6.1.0
 sleep 30
 # for mod in $list; do
