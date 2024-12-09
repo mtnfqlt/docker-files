b83f65454003d18fa77fe8318637eca3a09f1ccc
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-09 20:14:59 +0200

    test

--- setup.sh
+++ setup.sh
@@ -9,5 +9,5 @@ apt-get full-upgrade -y
 apt-get install -y --no-install-recommends apt-utils jq
 
 echo "$setup_list" | jq -r 'to_entries[] | "\(.key) \(.value)"' | while read -r key value; do
-  "./$key/setup.sh" "$value"
+  eval "./$key/setup.sh $value"
 done
