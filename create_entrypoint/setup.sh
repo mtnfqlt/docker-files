06dc99a312f3b627f85d78ca98d21ac5b0a4b411
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-10 12:47:28 +0200

    test

--- create_entrypoint/setup.sh
+++ create_entrypoint/setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -e
 
+printf '\033[1;32m%s\033[0m\n' "$0"
+
 # shellcheck disable=SC1091
 source ./include.src
 
 # shellcheck disable=SC2154
-echo "$main_ps"
+cat > ./init.sh << EOT
+exec "$main_ps"
+EOT
--- init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-
-sleep 3600
-
