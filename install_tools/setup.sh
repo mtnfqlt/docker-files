7c5351749a0043d60ce35465b3f10deefebc690c
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-11 01:15:30 +0200

    test

--- install_tools/setup.sh
+++ install_tools/setup.sh
@@ -6,15 +6,13 @@ printf '\033[1;32m%s\033[0m\n' "$0"
 source ./include.src
 
 # shellcheck disable=SC2154
-list="$list
-  curl
-  dnsutils
-  iproute2
-  iputils-ping
-  mc
-  netcat-traditional
-  net-tools
-  procps
-  wget"
-
-eval apt-get install -y --no-install-recommends "$list"
+eval apt-get install -y --no-install-recommends "$list" \
+  curl \
+  dnsutils \
+  iproute2 \
+  iputils-ping \
+  mc \
+  netcat-traditional \
+  net-tools \
+  procps \
+  wget
