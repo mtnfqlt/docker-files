a38a63b691bfa16d235a1b9c2ac430213c13759b
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-10 12:49:48 +0200

    test

--- create_entrypoint/setup.sh
+++ create_entrypoint/setup.sh
@@ -2,10 +2,14 @@
 
 printf '\033[1;32m%s\033[0m\n' "$0"
 
+init_script='./init.sh'
+
 # shellcheck disable=SC1091
 source ./include.src
 
 # shellcheck disable=SC2154
-cat > ./init.sh << EOT
+cat > $init_script << EOT
 exec "$main_ps"
 EOT
+
+chmod 700 $init_script
