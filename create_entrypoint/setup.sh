8a8e3cdf1e4adcb836a073d03e9b39b4e99586db
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-10 19:57:17 +0200

    test

--- create_entrypoint/setup.sh
+++ create_entrypoint/setup.sh
@@ -8,57 +8,57 @@ init_script='./init.sh'
 source ./include.src
 
 # shellcheck disable=SC2154
-cat > $init_script << EOT
-#!/bin/bash -e
+# cat > $init_script << EOT
+# #!/bin/bash -e
 
-ctl_port=$ctl_port
-main_ps='$main_ps'
+# ctl_port=$ctl_port
+# main_ps='$main_ps'
 
-exec_on_exit() {
-  stop_main_ps
-}
+# exec_on_exit() {
+#   stop_main_ps
+# }
 
-start_main_ps() {
-  setsid \$main_ps &
-  main_pid=\$!
-  printf '\033[1;32mThe main process was successfully started (PID:%s).\033[0m\n' \$main_pid
-}
+# start_main_ps() {
+#   setsid \$main_ps &
+#   main_pid=\$!
+#   printf '\033[1;32mThe main process was successfully started (PID:%s).\033[0m\n' \$main_pid
+# }
 
-stop_main_ps() {
-  if [ -n "\$main_pid" ]; then
-    kill "\$main_pid"
-    printf '\033[1;33mThe main process was terminated (PID:%s).\033[0m\n' \$main_pid
-  fi
-}
+# stop_main_ps() {
+#   if [ -n "\$main_pid" ]; then
+#     kill "\$main_pid"
+#     printf '\033[1;33mThe main process was terminated (PID:%s).\033[0m\n' \$main_pid
+#   fi
+# }
 
-restart_main_ps() {
-  stop_main_ps
-  start_main_ps
-}
+# restart_main_ps() {
+#   stop_main_ps
+#   start_main_ps
+# }
 
-enable_php_mod() {
-  local mod=\$1
+# enable_php_mod() {
+#   local mod=\$1
 
-  docker-php-ext-enable "\$mod"
-  restart_main_ps
-}
+#   docker-php-ext-enable "\$mod"
+#   restart_main_ps
+# }
 
-disable_php_mod() {
-  local mod=\$1
+# disable_php_mod() {
+#   local mod=\$1
 
-  rm -f "/usr/local/etc/php/conf.d/docker-php-ext-\$mod.ini"
-  restart_main_ps
-}
+#   rm -f "/usr/local/etc/php/conf.d/docker-php-ext-\$mod.ini"
+#   restart_main_ps
+# }
 
-trap exec_on_exit EXIT
+# trap exec_on_exit EXIT
 
-ip=\$(ifconfig eth0 | grep ' inet ' | awk '{print \$2}')
-echo "\$ip:\$ctl_port"
-start_main_ps
+# ip=\$(ifconfig eth0 | grep ' inet ' | awk '{print \$2}')
+# echo "\$ip:\$ctl_port"
+# start_main_ps
 
-while true; do
-  eval "\$(nc -lp \$ctl_port)" || true
-done
-EOT
+# while true; do
+#   eval "\$(nc -lp \$ctl_port)" || true
+# done
+# EOT
 
 chmod 700 $init_script
--- init.sh
+++ init.sh
@@ -2,6 +2,53 @@
 
 printf '\033[1;32m%s\033[0m\n' "$0"
 
+#!/bin/bash -e
+
+ctl_port=54321
+main_ps='apache2-foreground'
+
+exec_on_exit() {
+  stop_main_ps
+}
+
+start_main_ps() {
+  setsid $main_ps &
+  main_pid=$!
+  printf '\033[1;32mThe main process was successfully started (PID:%s).\033[0m\n' $main_pid
+}
+
+stop_main_ps() {
+  if [ -n "$main_pid" ]; then
+    kill "$main_pid"
+    printf '\033[1;33mThe main process was terminated (PID:%s).\033[0m\n' $main_pid
+  fi
+}
+
+restart_main_ps() {
+  stop_main_ps
+  start_main_ps
+}
+
+enable_php_mod() {
+  local mod=$1
+
+  docker-php-ext-enable "$mod"
+  restart_main_ps
+}
+
+disable_php_mod() {
+  local mod=$1
+
+  rm -f "/usr/local/etc/php/conf.d/docker-php-ext-$mod.ini"
+  restart_main_ps
+}
+
+trap exec_on_exit EXIT
+
+ip=$(ifconfig eth0 | grep ' inet ' | awk '{print $2}')
+echo "$ip:$ctl_port"
+start_main_ps
+
 while true; do
-  sleep 1
+  eval "$(nc -lp $ctl_port)" || true
 done
