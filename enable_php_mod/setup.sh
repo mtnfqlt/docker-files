1b85d99a6b9dbb66da03c1aad37854eee55a07aa
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-11 01:33:37 +0200

    test

--- enable_php_mod/setup.sh
+++ enable_php_mod/setup.sh
@@ -5,5 +5,4 @@ printf '\033[1;32m%s\033[0m\n' "$0"
 # shellcheck disable=SC1091
 source ./include.src
 
-# shellcheck disable=SC2154
-eval docker-php-ext-enable "$list"
+if [ -n "$list" ]; then eval docker-php-ext-enable "$list"; fi
--- install_php_mod/setup.sh
+++ install_php_mod/setup.sh
@@ -5,5 +5,4 @@ printf '\033[1;32m%s\033[0m\n' "$0"
 # shellcheck disable=SC1091
 source ./include.src
 
-# shellcheck disable=SC2154
-eval pecl install "$list"
+if [ -n "$list" ]; then eval pecl install "$list"; fi
