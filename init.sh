6abab497c76d6ea7d4be2d8c798b148b2f04efe2
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-10 22:40:20 +0200

    test

--- init.sh
+++ init.sh
@@ -2,6 +2,8 @@
 
 printf '\033[1;32m%s\033[0m\n' "$0"
 
+work_dir=$(dirname "$(realpath "$0")")
+
 exec_on_exit() {
   stop_main_ps
 }
@@ -9,13 +11,13 @@ exec_on_exit() {
 start_main_ps() {
   setsid "$MAIN_PS" &
   main_pid=$!
-  printf '\033[1;32mThe main process was successfully started (PID:%s).\033[0m\n' $main_pid
+  printf '\033[1;32mThe main process was successfully started (PID:%s).\033[0m\n' "$main_pid"
 }
 
 stop_main_ps() {
   if [ -n "$main_pid" ]; then
     kill "$main_pid"
-    printf '\033[1;33mThe main process was terminated (PID:%s).\033[0m\n' $main_pid
+    printf '\033[1;33mThe main process was terminated (PID:%s).\033[0m\n' "$main_pid"
   fi
 }
 
@@ -40,9 +42,18 @@ disable_php_mod() {
 
 trap exec_on_exit EXIT
 
-ip=$(ifconfig eth0 | grep ' inet ' | awk '{print $2}')
-echo "$ip:$CTL_PORT"
-start_main_ps
+cd "$work_dir"
+echo "$(ifconfig eth0 | grep ' inet ' | awk '{print $2}'):$CTL_PORT"
+
+setup_script='/mnt/setup.sh'
+if [ -f $setup_script ]; then $setup_script; fi
+cd "$work_dir"
+
+for file in ./init.d/*; do
+  $file
+done
+
+if [ -n "$MAIN_PS" ]; then start_main_ps; fi
 
 while true; do
   eval "$(nc -lp "$CTL_PORT")" || true
