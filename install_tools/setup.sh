4eaa5592ae3b3d7c8e5859302536c73e2ca7aa04
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-10 03:16:51 +0200

    test

--- install_tools/setup.sh
+++ install_tools/setup.sh
@@ -6,8 +6,7 @@ printf '\033[1;32m%s\033[0m\n' "$0"
 source ./include.src
 
 # shellcheck disable=SC2154
-echo "$list"
-apt-get install -y --no-install-recommends $list \
+eval apt-get install -y --no-install-recommends "$list" \
   dnsutils \
   iproute2 \
   iputils-ping \
