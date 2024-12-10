d6ae7c19563327834495a20246a8e4474691aed2
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-10 23:20:41 +0200

    test

--- init.sh
+++ init.sh
@@ -11,12 +11,14 @@ exec_on_exit() {
 start_main_ps() {
   setsid "$MAIN_PS" &
   main_pid=$!
+  sleep 3
   printf '\033[1;32mThe main process was successfully started (PID:%s).\033[0m\n' "$main_pid"
 }
 
 stop_main_ps() {
   if [ -n "$main_pid" ]; then
     kill "$main_pid"
+    sleep 3
     printf '\033[1;33mThe main process was terminated (PID:%s).\033[0m\n' "$main_pid"
   fi
 }
@@ -52,8 +54,8 @@ cd "$work_dir"
 init_list=$(find ./init.d -maxdepth 1 -type f -name '*.sh' | sort -V)
 
 if [ -z "$MAIN_PS" ]; then
-  init_list=$(echo "$init_list" | head -n -1)
   MAIN_PS=$(echo "$init_list" | tail -n -1)
+  init_list=$(echo "$init_list" | head -n -1)
 fi
 
 for init in $init_list; do
