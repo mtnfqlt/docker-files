e188b65fb8bdc66e0dd5b3422330de2a82d07064
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-11 19:04:00 +0200

    test

--- init.sh
+++ init.sh
@@ -43,7 +43,7 @@ trap exec_on_exit EXIT
 
 cd "$work_dir"
 ifconfig eth0 | grep ' inet ' | awk '{print $2}'
-if [ "${1#-}" = "$1" ]; then CMD=$(printf %s "$@"); fi
+if [ "${1#-}" = "$1" ]; then CMD=$(printf '%s' "$@"); fi
 
 setup_script='/mnt/setup.sh'
 if [ -f $setup_script ]; then $setup_script; fi
