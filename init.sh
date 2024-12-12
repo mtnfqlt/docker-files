d2b6662d51974255d4948f01a02c2b4c10b33dfc
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-12 21:48:51 +0200

    test

--- init.sh
+++ init.sh
@@ -4,16 +4,54 @@ printf '\033[1;32m%s\033[0m\n' "$0"
 
 work_dir=$(dirname "$(realpath "$0")")
 
+exec_on_exit() {
+  stop_main_init
+}
+
+start_main_init() {
+  if [ -n "$MAIN_INIT" ]; then
+    printf '\033[1;32m%s\033[0m\n' "${FUNCNAME[0]}"
+    $MAIN_INIT &
+    main_init_pid=$!
+  fi
+}
+
+stop_main_init() {
+  if [ -n "$main_init_pid" ]; then
+    printf '\033[1;33m%s\033[0m\n' "${FUNCNAME[0]}"
+    kill "$main_init_pid"
+
+    while kill -0 "$main_init_pid" 2>/dev/null; do
+      sleep 1
+    done
+  fi
+}
+
+restart_main_init() {
+  stop_main_init
+  start_main_init
+}
+
+trap exec_on_exit EXIT
+
 cd "$work_dir"
 ifconfig eth0 | grep ' inet ' | awk '{print $2}'
 
-echo "$MAIN_INIT"
-$MAIN_INIT &
-echo "$*"
-"$@" &
+init_script_list=$(find ./init.d -maxdepth 1 -type f -name '*.sh' | sort -V)
+
+for init_script in $init_script_list; do
+  $init_script &
+done
 
 if [ -n "$*" ]; then MAIN_INIT="$*"; fi
-$MAIN_INIT &
+start_main_init
+
+bind_ip='127.0.0.1'
+if [ "$EN_RCTL" = true ]; then bind_ip=''; fi
+
+while true; do
+  eval "$(nc -l $bind_ip -p "$RCTL_PORT")" || true
+done
 
 
 # exec_on_exit() {
