995e4aa7d1475827823cdba7e78e940c5b084c8e
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-12 03:42:12 +0200

    test

--- init.sh
+++ init.sh
@@ -9,14 +9,18 @@ exec_on_exit() {
 }
 
 start_cmd() {
-  printf '\033[1;32m%s\033[0m\n' "${FUNCNAME[0]}"
-  $CMD &
-  cmd_pid=$!
+  if [ -n "$CMD" ]; then
+    printf '\033[1;32m%s\033[0m\n' "${FUNCNAME[0]}"
+    $CMD &
+    cmd_pid=$!
+  fi
 }
 
 stop_cmd() {
-  printf '\033[1;33m%s\033[0m\n' "${FUNCNAME[0]}"
-  if [ -n "$cmd_pid" ]; then kill "$cmd_pid"; fi
+  if [ -n "$cmd_pid" ]; then
+    printf '\033[1;33m%s\033[0m\n' "${FUNCNAME[0]}"
+    kill "$cmd_pid"
+  fi
 }
 
 restart_cmd() {
@@ -37,16 +41,11 @@ esac
 
 init_script_list=$(find ./init.d -maxdepth 1 -type f -name '*.sh' | sort -V)
 
-# if [ -z "$CMD" ]; then
-#   CMD=$(echo "$init_script_list" | tail -n -1)
-#   init_script_list=$(echo "$init_script_list" | head -n -1)
-# fi
-
 for init_script in $init_script_list; do
   $init_script &
 done
 
-if [ -n "$CMD" ]; then start_cmd; fi
+start_cmd
 
 while true; do
   eval "$(nc -lp "$CTL_PORT")" || true
