a3fa67da1206b63f073c8be76d72b6e954f39c1b
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-13 18:02:09 +0200

    test

--- make.sh
+++ make.sh
@@ -12,7 +12,7 @@ chown $login_user:$login_user ./
 if [ -n "$(find ./ -maxdepth 0 -empty)" ]; then
   if [ -n "$REPO_URL" ]; then sudo -u $login_user git clone "$REPO_URL" ./
     if [ -n "$BRANCH" ]; then sudo -u $login_user git checkout "$BRANCH"; fi
-    chmod -R g+rw ./
     if [ -n "$INIT_SCRIPT" ]; then sudo -u $login_user "$INIT_SCRIPT"; fi
+    chmod -R g+rw ./
   fi
 fi
