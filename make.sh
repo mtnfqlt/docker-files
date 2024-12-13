8103e3c311bd472cc582c91471736eff9fc5a270
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-13 15:06:35 +0200

    test

--- make.sh
+++ make.sh
@@ -2,15 +2,18 @@
 
 printf '\033[1;32m%s\033[0m\n' "$0"
 
-work_dir='/home/project/src'
+login_user='project'
+work_dir="/home/$login_user/src"
 
 mkdir -p $work_dir
 cd $work_dir
-chown project:project ./
+chown $login_user:$login_user ./
 
 if [ -n "$(find ./ -maxdepth 0 -empty)" ]; then
-  if [ -n "$REPO_URL" ]; then sudo -Eu project git clone "$REPO_URL" ./
-    if [ -n "$BRANCH" ]; then sudo -Eu project git checkout "$BRANCH"; fi
-    if [ -n "$INIT_SCRIPT" ]; then sudo -Eu project "$INIT_SCRIPT"; fi
+  if [ -n "$REPO_URL" ]; then sudo -Eu $login_user git clone "$REPO_URL" ./
+    if [ -n "$BRANCH" ]; then sudo -Eu $login_user git checkout "$BRANCH"; fi
+    if [ -n "$INIT_SCRIPT" ]; then sudo -Eu $login_user "$INIT_SCRIPT"; fi
   fi
 fi
+
+chmod -R g+rw ./
