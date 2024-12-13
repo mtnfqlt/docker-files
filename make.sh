140e872c862a9db256f6451295af687ea8bb2879
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-13 15:35:30 +0200

    test

--- make.sh
+++ make.sh
@@ -13,8 +13,8 @@ if [ -n "$(find ./ -maxdepth 0 -empty)" ]; then
   if [ -n "$REPO_URL" ]; then sudo -u $login_user git clone "$REPO_URL" ./
     if [ -n "$BRANCH" ]; then sudo -u $login_user git checkout "$BRANCH"; fi
     chmod -R g+rw ./
-    # curl -sSL 'https://getcomposer.org/download/latest-2.x/composer.phar' > /usr/local/bin/composer
-    # chmod a+x /usr/local/bin/composer
+    curl -sSL 'https://getcomposer.org/download/latest-2.x/composer.phar' > /usr/local/bin/composer
+    chmod a+x /usr/local/bin/composer
     if [ -n "$INIT_SCRIPT" ]; then sudo -u $login_user "$INIT_SCRIPT"; fi
   fi
 fi
