2449b387a4d9a48f2081b24dca3db950078b27d3
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-14 23:22:35 +0200

    test

--- rctl.sh
+++ rctl.sh
@@ -5,18 +5,18 @@ printf '\033[1;32m%s\033[0m\n' "$0"
 # shellcheck disable=SC1091
 source ./include.src
 
-rpctl_port=54321
+rctl_port=54321
 
 # shellcheck disable=SC2154
 for php_ext in $enable_php_ext; do
-cat << EOT | nc -q1 php-fpm $rpctl_port
+cat << EOT | nc -q1 php-fpm $rctl_port
 echo enable $php_ext
 EOT
 done
 
 # shellcheck disable=SC2154
 for php_ext in $disable_php_ext; do
-cat << EOT | nc -q1 php-fpm $rpctl_port
+cat << EOT | nc -q1 php-fpm $rctl_port
 echo disable $php_ext
 EOT
 done
