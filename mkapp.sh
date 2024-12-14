5099596c12b96649ff8c302e7e341d1ec2bb9260
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-14 02:02:47 +0200

    test

--- mkapp.sh
+++ mkapp.sh
@@ -2,7 +2,7 @@
 
 printf '\033[1;32m%s\033[0m\n' "$0"
 
-login_user='project'
+login_user=$(getent passwd 1000 | cut -d: -f1)
 work_dir="/home/$login_user/src"
 
 exec_on_exit() {
@@ -12,13 +12,13 @@ exec_on_exit() {
 
 trap exec_on_exit EXIT
 
-mkdir -p $work_dir
-cd $work_dir
-chown $login_user:$login_user ./
+mkdir -p "$work_dir"
+cd "$work_dir"
+chown "$login_user:$login_user" ./
 
 if [ -n "$(find ./ -maxdepth 0 -empty)" ]; then
-  if [ -n "$REPO_URL" ]; then sudo -u $login_user git clone "$REPO_URL" ./
-    if [ -n "$BRANCH" ]; then sudo -u $login_user git checkout "$BRANCH"; fi
-    if [ -n "$INIT_SCRIPT" ]; then sudo -u $login_user "$INIT_SCRIPT"; fi
+  if [ -n "$REPO_URL" ]; then sudo -u "$login_user" git clone "$REPO_URL" ./
+    if [ -n "$BRANCH" ]; then sudo -u "$login_user" git checkout "$BRANCH"; fi
+    if [ -n "$INIT_SCRIPT" ]; then sudo -u "$login_user" "$INIT_SCRIPT"; fi
   fi
 fi
