ba291e70f19d29f357de781bd8789fc8a286d10f
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-16 20:46:52 +0200

    test

--- addhost.sh
+++ addhost.sh
@@ -35,7 +35,7 @@ getent hosts $domain"
   sudo bash -ec "$cmd"
   vm_name='dvm'
 
-  if multipass info $vm_name | grep -q '^State:\s*Running$'; then
+  if multipas info $vm_name | grep -q '^State:\s*Running$'; then
     echo
     multipass exec $vm_name -- sudo bash -ec "$cmd"
   fi
