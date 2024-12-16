58ab810e6e254247f6f0ae40f543bd0f154fd11f
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-16 23:28:06 +0200

    test

--- addhost.sh
+++ addhost.sh
@@ -4,10 +4,10 @@ printf '\033[1;32m%s\033[0m\n' "$1"
 
 
 run_on_dvm() {
-  multipass exec dvm -- eval "ls"
+  multipass exec dvm -- bash -ec "ls"
 }
 
-route_list=$(run_on_dvm)
+route_list="$(run_on_dvm)"
 echo "$route_list"
 
 
