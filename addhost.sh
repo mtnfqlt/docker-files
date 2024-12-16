23515d8006f610aefcc4db6401fd8025cad73775
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-16 22:38:51 +0200

    test

--- addhost.sh
+++ addhost.sh
@@ -4,7 +4,7 @@ printf '\033[1;32m%s\033[0m\n' "$1"
 
 work_dir=$(dirname "$(realpath "$1")")
 cur_script=$(realpath "$1")
-prj_config='docker-compose.yml'
+prj_config='./docker-compose.yml'
 
 exec_on_exit() {
   if [ $? -ne 0 ]; then printf '\033[1;31m%s\033[0m\n' "$(basename "$cur_script")"; fi
@@ -22,14 +22,12 @@ run_on_dvm() {
 trap exec_on_exit EXIT
 
 cd "$work_dir"
-prj_name=$(yq -r '.name' ./$prj_config)
+prj_name=$(yq -r '.name' $prj_config)
 if [ -z "$prj_name" ]; then prj_name=$(basename "$$work_dir"); fi
-
-service=$(yq -r '.services | to_entries[] | select(.value.environment | has("DOMAIN")) | .key' ./$prj_config)
+service=$(yq -r '.services | to_entries[] | select(.value.environment | has("DOMAIN")) | .key' $prj_config)
 container="$prj_name-$service-1"
-
 cmd="docker exec $container ip route"
-run_on_dvm "$cmd" || true
+run_on_dvm "$cmd" 2> /dev/null || true
 
 
 # gateway=$(docker exec "$container" ip route | grep '^default via ' | awk '{print $3}')
