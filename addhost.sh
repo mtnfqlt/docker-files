ffc565264add0c2aeb977452d9353fae83ffe46b
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-17 00:54:45 +0200

    test

--- addhost.sh
+++ addhost.sh
@@ -28,6 +28,7 @@ prj_name=$(yq -r '.name' $prj_config)
 if [ -z "$prj_name" ]; then prj_name=$(basename "$$work_dir"); fi
 service=$(yq -r '.services | to_entries[] | select(.value.environment | has("DOMAIN")) | .key' $prj_config)
 cmd="set -e; docker exec $prj_name-$service-1 ip route"
+cmd="set -e; ls"
 route_list=$(run_on_dvm "$cmd" 2> /dev/null)
 echo aaa
 bash -ec "$cmd"
