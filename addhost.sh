09177da9d2a505253809562847783b8a1ae275cc
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-16 14:18:08 +0200

    test

--- addhost.sh
+++ addhost.sh
@@ -5,3 +5,7 @@ prj_name=$(docker compose config | yq -r '.name')
 if [ -z "$prj_name" ]; then prj_name=$(basename "$(dirname "$(realpath "$0")")"); fi
 
 echo "$prj_name"
+
+network="$prj_name\_default"
+
+echo "$network"
