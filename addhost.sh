42b737c38368881be6ba49485a93fc328f55d220
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-16 19:39:50 +0200

    test

--- addhost.sh
+++ addhost.sh
@@ -30,8 +30,8 @@ if [ -n "$gateway" ] && [ -n "$domain" ]; then
 set -e
 hostname
 cp /etc/hosts /etc/hosts.$(date +%F_%T)
-sed -i '/ $domain /{N;d;}' /etc/hosts
-echo $gateway $domain \#added by $script >> /etc/hosts
+sed -i '/ $domain /d' /etc/hosts
+#echo $gateway $domain \#added by $script >> /etc/hosts
 cat /etc/hosts
 "
   #eval "$cmd"
