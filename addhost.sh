14de253040f5f6338baf0e0b0776c1a6ccbcd5a2
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-16 18:45:39 +0200

    test

--- addhost.sh
+++ addhost.sh
@@ -31,7 +31,7 @@ hostname
 backups_dir='/etc/hosts_backups'
 
 mkdir -p \$backups_dir
-cp /etc/hosts \$backups_dir\\$(date +%F_%T)
+cp /etc/hosts \$backups_dir\aaa
 echo $gateway $domain \#added by $script
 "
   eval "$cmd"
