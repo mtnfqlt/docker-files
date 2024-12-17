2b111247608b48cc113a6365c8401273d25f25f4
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-17 16:22:50 +0200

    test

--- addhost.sh
+++ addhost.sh
@@ -29,7 +29,7 @@ cmd="docker exec $prj_name-$service-1 ip route | grep '^default via ' | awk '{pr
 vm_ip=$(multipass info $vm_name --format json 2> /dev/null | jq -r ".info.$vm_name.ipv4[0]")
 
 if [ -n "$vm_ip" ]; then
-  exec_on_dvm "$cmd"
+  gateway=$(exec_on_dvm "$cmd")
 else
   gateway=$(eval "$cmd")
 fi
