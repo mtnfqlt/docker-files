f043588b985eb2a8e2504b65ee29131c1e0d4994
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-17 03:41:49 +0200

    test

--- addhost.sh
+++ addhost.sh
@@ -22,6 +22,7 @@ cmd="docker exec $prj_name-$service-1 ip route"
 if multipass info $vm_name 2> /dev/null | grep -q '^State:\s*Running$'; then
   multipass exec $vm_name -- 'ls'
 fi
+echo aaa
 echo "$route_list"
 if [ -z "$route_list" ]; then route_list=$(eval "$cmd"); fi
 gateway=$(echo "$route_list" | grep '^default via ' | awk '{print $3}')
