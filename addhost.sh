1247f1ea54655c2fde2711845c5cfbc0b5c323b9
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-17 17:47:23 +0200

    test

--- addhost.sh
+++ addhost.sh
@@ -41,8 +41,8 @@ sudo sed -i.bak '/ $domain /d' ./hosts
 sudo echo $gateway $domain \# Added by $cur_script at $(date +%F' '%T) >> ./hosts
 grep ' $domain ' ./hosts"
 
+  if ! exec_on_dvm "$cmd" | grep -E "^$gateway $domain #"; then printf '\033[1;33m%s not running\033[0m\n' "$vm_name"; fi
   if ! sudo bash -c "$cmd" | grep -E "^$gateway $domain #"; then exit 1; fi
-  exec_on_dvm "$cmd"
 else
   exit 1
 fi
