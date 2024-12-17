fc140525092d2f00d293d4f4c3ced84908683577
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-17 18:39:33 +0200

    test

--- addhost.sh
+++ addhost.sh
@@ -14,11 +14,9 @@ exec_on_exit() {
 exec_on_dvm(){
   local cmd=$1
 
-  if [ "$vm_ip" != 'null' ]; then
-    ssh -o StrictHostKeyChecking=no \
-        -o UserKnownHostsFile=/dev/null \
-        -o LogLevel=ERROR "ubuntu@$vm_ip" "sudo bash -c \"$cmd\"" < /dev/null
-  fi
+  ssh -o StrictHostKeyChecking=no \
+      -o UserKnownHostsFile=/dev/null \
+      -o LogLevel=ERROR "ubuntu@$vm_ip" "sudo bash -c \"$cmd\"" < /dev/null
 }
 
 trap exec_on_exit EXIT
@@ -29,8 +27,14 @@ if [ -z "$prj_name" ]; then prj_name=$(basename "$$work_dir"); fi
 service=$(yq -r '.services | to_entries[] | select(.value.environment | has("DOMAIN")) | .key' $prj_config)
 cmd="docker exec $prj_name-$service-1 ip route"
 vm_ip=$(multipass info $vm_name --format json 2> /dev/null | jq -r ".info.$vm_name.ipv4[0]")
-route_list=$(exec_on_dvm "$cmd")
-if [ -z "$route_list" ]; then route_list=$(eval "$cmd"); fi
+
+if [ "$vm_ip" != 'null' ]; then
+  route_list=$(exec_on_dvm "$cmd")
+else
+  printf '\033[1;33m%s not running\033[0m\n' "$vm_name"
+  route_list=$(eval "$cmd")
+fi
+
 gateway=$(echo "$route_list" | grep '^default via ' | awk '{print $3}')
 domain=$(yq -r '.services[] | select(.environment.DOMAIN) | .environment.DOMAIN' $prj_config)
 
@@ -42,7 +46,13 @@ sudo echo $gateway $domain \# Added by $cur_script at $(date +%F' '%T) >> ./host
 grep ' $domain ' ./hosts"
 
   printf '\033[1;32m%s\033[0m\n' "$vm_name"
-  exec_on_dvm "$cmd"
+
+  if [ "$vm_ip" != 'null' ]; then
+    exec_on_dvm "$cmd"
+  else
+    printf '\033[1;33m%s not running\033[0m\n' "$vm_name"
+  fi
+
   printf '\033[1;32m%s\033[0m\n' "$(hostname)"
   if ! sudo bash -c "$cmd" | grep -E "^$gateway $domain #"; then exit 1; fi
 else
