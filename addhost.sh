0b7692fbeef3f472a03a07c94e08d179807844ca
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-17 02:24:24 +0200

    test

--- addhost.sh
+++ addhost.sh
@@ -27,7 +27,7 @@ cd "$work_dir"
 prj_name=$(yq -r '.name' $prj_config)
 if [ -z "$prj_name" ]; then prj_name=$(basename "$$work_dir"); fi
 service=$(yq -r '.services | to_entries[] | select(.value.environment | has("DOMAIN")) | .key' $prj_config)
-cmd="docker exec $prj_name-$service-1 ip route"
+cmd="docker exec $prj_name-$service-1 ping -c1 10.24.0.1; ip route"
 route_list=$(exec_on_dvm "$cmd" 2> /dev/null)
 if [ -z "$route_list" ]; then route_list=$(eval "$cmd"); fi
 gateway=$(echo "$route_list" | grep '^default via ' | awk '{print $3}')
