fd755e75bbcb1dedfd4d24de0ba661c014a5d1a1
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-17 19:10:14 +0200

    test

--- addhost.sh
+++ addhost.sh
@@ -27,7 +27,7 @@ if [ -z "$prj_name" ]; then prj_name=$(basename "$$work_dir"); fi
 service=$(yq -r '.services | to_entries[] | select(.value.environment | has("DOMAIN")) | .key' $prj_config)
 cmd="docker exec $prj_name-$service-1 ip route"
 vm_ip=$(multipass info $vm_name --format json 2> /dev/null | jq -r ".info.$vm_name.ipv4[0]")
-echo "$vm_ip"
+
 if [[ "$vm_ip" =~ ^[1-9] ]]; then
   route_list=$(exec_on_dvm "$cmd")
 else
