be65f64e27ffdad3ab26527fcd67b125ff8b6135
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-17 15:51:29 +0200

    test

--- addhost.sh
+++ addhost.sh
@@ -11,6 +11,15 @@ exec_on_exit() {
   if [ $? -ne 0 ]; then printf '\033[1;31m%s\033[0m\n' "$cur_script"; fi
 }
 
+# exec_on_dvm(){
+#   local vm_name='dvm'
+
+#   if multipass info $vm_name 2> /dev/null | grep -q '^State:\s*Running$'; then
+#     vm_ip
+#     #gateway=$(echo "$cmd" | multipass shell | grep '^default via ' | awk '{print $3}')
+#   fi
+# }
+
 trap exec_on_exit EXIT
 
 cd "$work_dir"
@@ -19,28 +28,31 @@ if [ -z "$prj_name" ]; then prj_name=$(basename "$$work_dir"); fi
 service=$(yq -r '.services | to_entries[] | select(.value.environment | has("DOMAIN")) | .key' $prj_config)
 cmd="docker exec $prj_name-$service-1 ip route"
 
-if multipass info $vm_name 2> /dev/null | grep -q '^State:\s*Running$'; then
-  gateway=$(echo "$cmd" | multipass shell | grep '^default via ' | awk '{print $3}')
-fi
+vm_ip=$(multipass info $vm_name --format json | jq -r ".info.$vm_name.ipv4[0]")
+echo "$vm_ip"
 
-if [ -z "$gateway" ]; then gateway=$(eval "$cmd" | grep '^default via ' | awk '{print $3}'); fi
-domain=$(yq -r '.services[] | select(.environment.DOMAIN) | .environment.DOMAIN' $prj_config)
-echo aaa
-if [ -n "$gateway" ] && [ -n "$domain" ]; then
-  cmd="
-set -e
-cd /etc
-sed -i.bak '/ $domain /d' ./hosts
-echo $gateway $domain \#added by $cur_script >> ./hosts
-hostname
-grep ' $domain ' ./hosts"
+# if multipass info $vm_name 2> /dev/null | grep -q '^State:\s*Running$'; then
+#   gateway=$(echo "$cmd" | multipass shell | grep '^default via ' | awk '{print $3}')
+# fi
 
-  sudo bash -ec "$cmd"
+# if [ -z "$gateway" ]; then gateway=$(eval "$cmd" | grep '^default via ' | awk '{print $3}'); fi
+# domain=$(yq -r '.services[] | select(.environment.DOMAIN) | .environment.DOMAIN' $prj_config)
 
-  if multipass info $vm_name 2> /dev/null | grep -q '^State:\s*Running$'; then
-    echo
-    echo "sudo bash -ec \"$cmd\"" | multipass shell
-  fi
-else
-  exit 1
-fi
+# if [ -n "$gateway" ] && [ -n "$domain" ]; then
+#   cmd="
+# set -e
+# cd /etc
+# sed -i.bak '/ $domain /d' ./hosts
+# echo $gateway $domain \#added by $cur_script >> ./hosts
+# hostname
+# grep ' $domain ' ./hosts"
+
+#   sudo bash -ec "$cmd"
+
+#   if multipass info $vm_name 2> /dev/null | grep -q '^State:\s*Running$'; then
+#     echo
+#     echo "sudo bash -ec \"$cmd\"" | multipass shell
+#   fi
+# else
+#   exit 1
+# fi
