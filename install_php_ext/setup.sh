b6d3d0178db02c98278a1016284aa171083e1287
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2024-12-18 15:06:20 +0200

    test

--- install_php_ext/setup.sh
+++ install_php_ext/setup.sh
@@ -5,4 +5,7 @@ printf '\033[1;32m%s\033[0m\n' "$0"
 # shellcheck disable=SC1091
 source ./include.src
 
-if [ -n "$list" ]; then eval docker-php-ext-install "$list"; fi
+# shellcheck disable=SC2154
+for ext in $list; do
+  eval docker-php-ext-install "$ext"
+done
